0cf4c4bd249caa4889e9d13f5418b375831b8757
unknown <migaraten@gmail.com>
2024-02-09 18:31:15 +0200

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
-import express from 'express'
+import express from 'express';
+import dbConnection from './DB/connection.js'
+import cors from "cors";
+
+import user from './routes/user.js'
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/api', user)
 
 const PORT = 4100 || process.env.PORT;
 
 
+
+
 app.listen(PORT, () => {
-    console.log(`server running on: ${PORT}`)
-})
+  console.log(`server running on: ${PORT}`);
+});
+
+
+
+
+// Connect to the MySQL server
+
+
