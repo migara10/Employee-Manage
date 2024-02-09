816da67c42c0a98fd877d761f04dbd3ce3f8719b
unknown <migaraten@gmail.com>
2024-02-09 18:45:44 +0200

    update app js and main js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,39 @@
 
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import LoginComponent from './Components/Login/LoginComponent';
+import RegisterComponent from './Components/Register/RegisterComponent';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// routers
+const router = createBrowserRouter([
+  {
+      path: '/',
+      element: <div>Home</div>
+  },
+  {
+      path: '/register',
+      element: <RegisterComponent />
+  },
+  {
+      path: '/login',
+      element: <LoginComponent />
+  },
+  
+  {
+      path: '*',
+      element: <div>page not found</div>
+  },
+])
+
+
 
 function App() {
 
   return (
     <div>
-      <Login />
+      <RouterProvider router={router}></RouterProvider>
     </div>
   )
 }
